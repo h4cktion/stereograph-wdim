85e2fb970e4045c2cd69da61b5cc615d35d44739
hacktion <matthieu.wandolski@gmail.com>
2023-10-05 12:27:58 +0200

    feat(misc): add style to the project page

--- src/components/project/Project.tsx
+++ src/components/project/Project.tsx
@@ -1,5 +1,5 @@
 import { BASE_URL } from "@/constants";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import { useQuery } from "react-query";
 
 const Project = () => {
@@ -16,10 +16,25 @@ const Project = () => {
   });
 
   return (
-    <>
-      {project && JSON.stringify(project)}
+    <div className="w-8/12 m-auto p-8 bg-slate-50 my-8 rounded-xl shadow-xl text-slate-500 relative">
+      {project && (
+        <>
+          <h1 className="text-xl text-center">Détail du projet {project.id}</h1>
+          <Link
+            to={"/projets"}
+            className="absolute top-4 right-4 p-2 bg-teal-100 text-teal-500 rounded-lg hover:shadow-md cursor-pointer"
+          >
+            Retour
+          </Link>
+          <div className="flex flex-col w-full justify-evenly gap-2 pt-6">
+            <span>Nom: {project.nom}</span>
+            <span>Desciption: {project.description}</span>
+            <span>Etape: {project.etape}</span>
+          </div>
+        </>
+      )}
       {isLoading && <span>Récupération du projet...</span>}
-    </>
+    </div>
   );
 };
 
